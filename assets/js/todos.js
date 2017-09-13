007de9fe8396cc93e007c18371605362790eff4c
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-13 15:33:55 -0600

    fade out and remove li from dom using remove callback

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,14 @@ $("li").click(function () {
 	//if completed
 	$(this).toggleClass("completed");
 });
+
+// click on x to delete today
+$("span").click(function(e) {
+	$(this).parent('li').fadeOut(
+		500, function() {
+			$(this).remove();
+		});
+
+	e.stopPropagation(); // stop bubbling
+
+});
