941a4f19863ad7b259101b3e5dc815a9c1a2f2ba
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-13 15:44:25 -0600

    add additional functionality using on methods

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,12 @@
 // Check off to-dos by clicking
-$("li").click(function () {
+// add on listener to parent
+$("ul").on("click", "li", function () {
 	//if completed
 	$(this).toggleClass("completed");
 });
 
 // click on x to delete today
-$("span").click(function(e) {
+$("ul").on("click", "span", function(e) {
 	$(this).parent('li').fadeOut(
 		500, function() {
 			$(this).remove();
@@ -13,4 +14,15 @@ $("span").click(function(e) {
 
 	e.stopPropagation(); // stop bubbling
 
+});
+
+$("input[type='text']").keypress(function(e) {
+	if (event.which === 13) { // enter key
+		var todoText = $(this).val();
+		$(this).val("");
+
+		$("ul").append("<li><span>x</span>"+todoText+"</li>");
+	}
+	// click() only adds listeners for existing elements
+	// on() will add listeners for all potential future elements
 });
