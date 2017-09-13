c541f03ba66a18871a92e5a359b3c85f3e994ae3
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-13 15:25:50 -0600

    change toggle to class

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,5 @@
 // Check off to-dos by clicking
 $("li").click(function () {
 	//if completed
-	console.log($(this).css("textDecoration"));
-	if ($(this).css("color") === "rgb(153, 153, 153)") {
-		$(this).css({
-			color: "rgb(102, 102, 102)",
-			textDecoration: "none"
-		});
-	} else {
-		$(this).css({
-			color: "rgb(153, 153, 153)",
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("completed");
 });
